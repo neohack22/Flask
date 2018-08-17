60a071c868fb36b5a963a8cf619a1e0d2123a44a
ethercourt <neohack22@hotmail.com>
2018-08-17 11:51:12 +0200

    error 404 iso 200

--- coucou.py
+++ coucou.py
@@ -164,11 +164,18 @@ def page_non_trouvee():
     return "Cette page devrait vous renvoyer une erreur 404"
 """
 
+"""
 @app.route('/404/')
 def page_non_trouvee():
     reponse = make_response("Cette page devrait vous avoir renvoyé une erreur 404")
     reponse.status_code = 404
     return reponse
+"""
 
+@app.route('/404')
+def page_non_trouvee():
+    reponse = make_response("Cette page devrait vous avoir renvoyé une erreur 404", 404)
+    return reponse
+    
 if __name__ == '__main__':
     app.run(debug=True)
